414dfeffa6f33d64b6f93d68b78f396fc77ff9e8
Mihir Shah <>
2022-11-19 16:50:49 +0000

    first assignment

--- main.cpp
+++ main.cpp
@@ -3,17 +3,16 @@
 using namespace std;
 
 int main() {
-  int a = 2,b = 3,c = 4,d = 5,x = 7;
+  int a = 2, b = 3, c = 4, d = 5, x = 7;
   a += 3;
   b -= 3;
   c *= 3;
   d /= 3;
   x %= 3;
-  cout << "a = " << a<<endl;
-  cout << "b = " << b<<endl;
-  cout << "c = " << c<<endl;
-  cout << "d = " << d<<endl;
-  cout << "x = " << x<<endl;
+  cout << "a = " << a << endl;
+  cout << "b = " << b << endl;
+  cout << "c = " << c << endl;
+  cout << "d = " << d << endl;
+  cout << "x = " << x << endl;
   return 0;
-
 }
