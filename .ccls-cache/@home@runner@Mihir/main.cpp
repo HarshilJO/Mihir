8c4a5bef3fe18b1a29db8d06364d37ef9c789d4f
Mihir Shah <>
2022-11-19 16:48:15 +0000

    first assignment

--- .ccls-cache/@home@runner@Mihir/main.cpp
+++ .ccls-cache/@home@runner@Mihir/main.cpp
@@ -3,14 +3,17 @@
 using namespace std;
 
 int main() {
-
-  //we are writing a cout program --> single line comments
-  /*My name is mihir,
-he is joining from mumbai.
-very nice person      --> multiple line comments
-*/ 
-  
-  cout << "Hello World!";
+  int a = 2,b = 3,c = 4,d = 5,x = 7;
+  a += 3;
+  b -= 3;
+  c *= 3;
+  d /= 3;
+  x %= 3;
+  cout << "a = " << a<<endl;
+  cout << "b = " << b<<endl;
+  cout << "c = " << c<<endl;
+  cout << "d = " << d<<endl;
+  cout << "x = " << x<<endl;
   return 0;
 
 }
