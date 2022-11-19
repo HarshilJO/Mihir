f38e4a5ce2d534de1affd3e6ea543611bbda0617
Mihir Shah <>
2022-11-19 16:32:17 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Mihir/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  //we are writing a cout program --> single line comments
+  /*My name is mihir,
+he is joining from mumbai.
+very nice person      --> multiple line comments
+*/ 
+  
+  cout << "Hello World!";
+  return 0;
+
+}
